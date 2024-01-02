33c8b87ecf1bfc2d3403da6906e228cdb89dee6e
Amine Benziane <aminebenziane@DIT000126.local>
2024-01-02 14:17:09 +0000

    fixewd build issue, using re_path with regex pattern

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('auth/', include('authbroker_client.urls', namespace='authbroker')),
     path('admin/', admin.site.urls),
-    path(r'^', include('scanner.urls')),
+    re_path(r'^', include('scanner.urls')),
 ]
--- scanner/urls.py
+++ scanner/urls.py
@@ -1,10 +1,10 @@
 
 from .views import (home_page)
 
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path(r'^$', RedirectView.as_view(pattern_name='home_page')),
+    re_path(r'^$', RedirectView.as_view(pattern_name='home_page')),
     path('home/', home_page.as_view(), name='home_page'),
 ]
