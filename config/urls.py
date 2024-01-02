103dc7f706a5ebd86ed71fbe9add586cb333d1ea
Amine Benziane <aminebenziane@DIT000126.local>
2024-01-02 14:05:42 +0000

    Updated url dependency

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include, url
+from django.urls import path, include
 
 urlpatterns = [
     path('auth/', include('authbroker_client.urls', namespace='authbroker')),
     path('admin/', admin.site.urls),
-    url(r'^', include('scanner.urls')),
+    path(r'^', include('scanner.urls')),
 ]
